6d21f1bb78a29020288e07dbbeef5aeeed46de46
unknown <maulanarizkiali@gmail.com>
2023-04-18 18:04:41 +0700

    initial folder

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <Flex alignItems="center" justifyContent="space-between" p="10px 1rem">
+      <Text>Test</Text>
+    </Flex>
+  );
+};
+
+export default Header;
